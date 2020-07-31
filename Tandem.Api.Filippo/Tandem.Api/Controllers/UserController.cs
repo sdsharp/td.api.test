b898bf87e712cef26d3bb41ee77bae046316bc53
Filippo <Filippo@SDSharp.com>
2020-07-31 08:46:25 -0700

    Added initial Swagger examples

--- Tandem.Api.Filippo/Tandem.Api/Controllers/UserController.cs
+++ Tandem.Api.Filippo/Tandem.Api/Controllers/UserController.cs
@@ -8,19 +8,51 @@ using Tandem.Domain.DTO.Users;
 
 namespace Tandem.Api.Controllers
 {
+    /// <summary>
+    /// Manages the endpoints for User features.
+    /// </summary>
     [ApiController]
-    [Route("[controller]")]
+    [Route("v1/[controller]")]
     public class UserController : ControllerBase
     {
         private readonly ILogger<UserController> _logger;
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="UserController"/> class.
+        /// </summary>
+        /// <param name="logger"></param>
         public UserController(ILogger<UserController> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Creates a new user.
+        /// </summary>
+        /// <returns>Returns the created user.</returns>
         [HttpPost]
-        public OutputUser Post(InputUser info)
+        public OutputUser Post([FromBody] InputUser request)
+        {
+            return new OutputUser();
+        }
+
+        /// <summary>
+        /// Gets user by email.
+        /// </summary>
+        /// <returns>Returns the found user.</returns>
+        [HttpGet("{emailAddress}")]
+        public OutputUser Get([FromRoute] String emailAddress)
+        {
+            var result = new OutputUser();
+            return result;
+        }
+
+        /// <summary>
+        /// Updates an existing user.
+        /// </summary>
+        /// <returns>Returns the updated user.</returns>
+        [HttpPut]
+        public OutputUser Put([FromRoute] Guid userId, [FromBody] InputUser request)
         {
             return new OutputUser();
         }
---
+++ Tandem.Api.Filippo/Tandem.Api/SwaggerExamples/InputUserExample.cs
@@ -0,0 +1,27 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+using Tandem.Domain.DTO.Users;
+
+namespace Tandem.Api.SwaggerExamples
+{
+    /// <summary>
+    /// Provides example data for <see cref="InputUser"/> class.
+    /// </summary>
+    public class InputUserExample : IExamplesProvider<InputUser>
+    {
+        /// <summary>
+        /// Get example data for <see cref="InputUser"/> class.
+        /// </summary>
+        /// <returns></returns>
+        public InputUser GetExamples()
+        {
+            return new InputUser
+            {
+                FirstName = "Mary",
+                MiddleName = "J",
+                LastName = "Poppins",
+                EmailAddress = "mary@elitechildcare.com",
+                PhoneNumber = "555-555-5555",
+            };
+        }
+    }
+}
